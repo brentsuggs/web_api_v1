34275d916b873044d0e75378d4fd18dca1227fe2
Brent S <bsuggs13@outlook.com>
2016-11-19 20:08:04 -0600

    First Draft

--- js/data.js
+++ js/data.js
@@ -1,9 +1,28 @@
 /****** VARIABLES *******/
-
-var deckURL = 'https://deckofcardsapi.com/api/deck/w11qmjea1ktq/shuffle/'; 
+var storage = localStorage; 
 var cardsURL = '';
-var deck = { };  
-var cards = [];
+var deckURL = '';
+var newDeck = false;
+var nowDate = new Date().getDate(); 
+var nowMonth = new Date().getMonth(); 
+
+// set deck ID from local storage: if exit and with the apis two week period then use deck ID 
+var myDeckID = storage.deckID; 
+if (myDeckID && (nowDate - storage.deckDate) < 14 && (nowMonth - storage.deckMonth) < 2) {
+    deckURL = 'https://deckofcardsapi.com/api/deck/' + myDeckID + '/shuffle/'; 
+
+} else { //else get a new deck ID 
+    deckURL = 'https://deckofcardsapi.com/api/deck/new/shuffle/'; 
+    newDeck = true;
+}
+
+var deck = {
+    "remaining" : 0,
+    "shuffled" : false,
+    "id" : "",
+    "cards" : []
+};  
+
 var $gallery = $('#gallery');
 
 
@@ -14,26 +33,58 @@ function createDeck(data) {
         deck.remaining = data.remaining; 
         deck.shuffled = data.shuffled; 
         deck.id = data.deck_id; 
-    } else {
         
+        //set deckDate if this is a new deck
+        if(newDeck) {
+            storage.deckDate = new Date().getDate();
+            storage.deckMonth = new Date().getMonth(); 
+        }
+        
+        //set cardsURL for ajax call
+        cardsURL = 'https://deckofcardsapi.com/api/deck/' + data.deck_id + '/draw/?count=52';
+        
+        //get all cards from deck 
+        $.getJSON(cardsURL, getCards); 
+        
+    } else {
+        $gallery.html('<p>Unable to get deck</p>');
     }
 }
-
 
 function getCards(data) {
+    cardsHTML = ' ';
     if(data.success === true) {
         $.each(data.cards, function(i, card){
-            cardsHTML = '<li id=" ' + card.code + ' " class="gallery-item">'; 
-            cardsHTMl += '<a href=" ' + card.image  + ' ">'
-            cardsHTML += '<img title'
+            //add card to deck
+            deck.cards.push(card);
+            
+            //build li elements for cards
+            cardsHTML += '<li id=" ' + card.code + ' " class="gallery-item">'; 
+            cardsHTML += '<a href=" ' + card.image + ' ">';
+            cardsHTML += '<img alt=" ' + card.value + ' OF ' + card.suit + ' " '; 
+            cardsHTML += 'src=" ' + card.image + ' " ';
+            cardsHTML += 'data-suit=" ' + card.suit + ' " ';
+            cardsHTML += 'data-value=" ' + card.value + ' " ';
+            cardsHTML += '>';
+            cardsHTML += '</a>';
+            cardsHTML += '</li>'
         });
+        $gallery.html(cardsHTML);
+        
+        // bind click event for #gallery links 
+        if ($('.gallery-item').length > 0) {
+            $('.gallery-item').click(function(e) {
+            e.preventDefault();
+            $(this).addClass('lightbox');
+            openLightbox( $('.lightbox')); 
+
+            });
+        }
+        
+        storage.deckID = deck.id; 
     }
 }
-
-
 
 
 //request a deck 
 $.getJSON(deckURL, createDeck); 
-
-$.getJSON(cardsURL, getCards); 
--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
 "use strict";
 
 //CREATE VARIABLES 
+var $search = $('#search'); 
 var $overlay = $('<div id="overlay"></div>'); 
 var $img = $('<img>'); 
-var $caption = $('<p></p>'); 
+var $info = $('<div id="info"></div>'); 
 var $lightboxContainer = $('<div id="lightboxContainer"></div>');
-var $close = $('<span id="navClose" class="icon-close"></span>');
-//var $leftArrow = $('<span id="navLeft" class="icon-reply"></span>');
-//var $rightArrow = $('<span id="navRight" class="icon-forward"></span>');
+var $close = $('<span id="navClose">Back</span>');
+var $leftArrow = $('<span id="navLeft" class="icon-reply"></span>');
+var $rightArrow = $('<span id="navRight" class="icon-forward"></span>');
 
 var $removed = []; 
+
+//append close to overlay 
+$lightboxContainer.append($close);
 
 //append img to the overlay
 $lightboxContainer.append($img);  
 
-//append caption to the overlay
-$lightboxContainer.append($caption); 
+//append info to the overlay
+$lightboxContainer.append($info); 
 
-//append close to overlay 
-$lightboxContainer.append($close); 
+//append info to the overlay
+$lightboxContainer.append($leftArrow, $rightArrow); 
 
 //append nav container to overlay 
 $overlay.append($lightboxContainer);
@@ -29,25 +33,27 @@ $('body').append($overlay);
 // Open Lightbox with selected image
 function openLightbox(item) {
     var href = item.children('a').attr('href');
-    var cap = item.children('a').children('img').attr('alt'); 
+    var infoHTML = '<h2>' + item.children('a').children('img').attr('alt') + '</h2>'; //title
+    infoHTML += '<ul>'; 
+    infoHTML += '<li> Card Value:  ' + item.children('a').children('img').attr('data-value') + '</li>';  
+    infoHTML += '<li> Card Suit:   ' + item.children('a').children('img').attr('data-suit') + '</li>'; 
+    infoHTML += '<li> Card Code:   ' + item.attr('id') + '</li>'; 
+    infoHTML += '</ul>'; 
     $img.attr('src', href); 
-    $caption.text(cap); 
+    $info.html(infoHTML); 
     $overlay.show('slow'); 
+    $('body').css('overflow', 'hidden');
 }
 
-// Get click event for #gallery links  
-$('.gallery-item').click(function(e) {
-    e.preventDefault();
-    $(this).addClass('lightbox');
-    openLightbox( $('.lightbox')); 
-
-});
-
-// Get click event for #overlay
-$close.click(function() {
+function closeLightbox() {
     $('.lightbox').removeClass('lightbox');
     $overlay.hide('slow'); 
-});
+    $('body').css('overflow', 'auto');
+}
+
+
+// Get click event for #overlay
+$close.click(closeLightbox);
 
 //Navigation of Lightbox 
 
@@ -74,29 +80,59 @@ function scrollLeft() {
     openLightbox($('.lightbox')); 
 }
 
+//keyboard navigation
+$rightArrow.click(function(e) {
+    scrollRight();
+});
 
-//$rightArrow.click(function(e) {
-//    scrollRight();
-//});
-//
-//$leftArrow.click(function(e) {
-//    scrollLeft();
-//});
-//
-////keyboard navigation
-//
-//function keyScroll(e) {
-//    if(e.which == 39) {
-//        scrollRight(); 
-//    }
-//    if(e.which == 37) {
-//        scrollLeft();
-//    }
-//}
-//
-//$overlay.keyup(function(e) {
-//    keyScroll(e); 
-//});
+$leftArrow.click(function(e) {
+    scrollLeft();
+});
+
+    
+function keyScroll(e) {
+    if(e.which == 39) {
+        scrollRight(); 
+    }
+    if(e.which == 37) {
+        scrollLeft();
+    }
+}
+
+$overlay.keyup(function(e) {
+    keyScroll(e); 
+});
+
+// Gallery Filtration 
+
+function filterImgs(item, search) {
+    var imgTitle = item.find('img').attr('alt').toLowerCase(); 
+    return imgTitle.indexOf(search); 
+}
+
+$search.keyup(function() {
+    var searchValue = $search.val().toLowerCase();
+    var result; 
+    $('.gallery-item').each(function () {
+        result =  filterImgs($(this), searchValue); 
+        if(result === -1) {
+            $removed.push($(this)); 
+            $(this).hide(700, function () {$(this).detach();}); 
+        }
+    });
+    
+    for (var i = 0; i < $removed.length; i++) {
+        result = filterImgs($removed[i], searchValue);
+        if(result !== -1) {
+            $gallery.append($removed[i]); 
+            $removed[i].show(700);
+            $removed.splice(i, 1);
+            i--; 
+        }
+    }
+    
+}); 
+
 
  
 
